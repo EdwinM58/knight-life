ed86ad117f72caca425cffa68909bce4bf004205
Mitchell-Lee98 <124094329+Mitchell-Lee98@users.noreply.github.com>
2023-02-01 20:17:20 -0500

    index.php
    working version of the frontend. Still needs to implement likes counter and a full header with logo

--- index.php
+++ index.php
@@ -8,41 +8,126 @@
 
 
 <head>
-	<meta charset="utf-8" />
+
+
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KnightLife</title>
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+
+
 	<style>
-		.post{
-			background-color: pink;
-			display: block;
-			margin-bottom: 1em;
-			width: 20em;
-			max-height: 30em;
-		}
-		.user{
-			text-decoration: underline;
-		}
-		.date{
-			color: red;
-			opacity: 0.4;
-		}
-		.date:hover{
-			opacity: 1;
+
+		html{
+			background-color:#1e1e1e;
 		}
 
-		/*.post{
-			border-style: soild;
-			display: block;
-			border-width: 1px;
-			border-color: black;
 
-			background-color: pink;
-			margin-bottom: 1em;
-		}*/
+		.posts{
+			color:white;
+    		display: flex;
+    		align-items: flex-start;
+    		padding-bottom: 10px;
+		}
+
+		.post__body{
+   			width: 450px;
+   			object-fit: contain;
+   			border-radius: 20px;
+		}
+
+		.post__Footer{
+			display: flex;
+   			margin-top: 10px;
+			margin-bottom: 10px;
+		}
+
+		.post__user{
+			font-weight: bold;
+   			font-size: 16px;
+    		margin-bottom: 5px;
+		}
+
+		.post__content{
+			text-indent: 30px;
+			margin-top: 10px;
+   		 	margin-bottom: 10px;
+   		 	font-size: 16px;
+		}
+
+		.postTime{
+			font-size: 12px;
+		}
+		
+		.post__line{
+			margin-top:10px;
+			margin-bottom:10px;
+			border-bottom: 1px solid black;
+		}
+
+		.tweetbox{
+   			padding-bottom: 10px;
+   			border-bottom: 8px;
+   			padding-right: 10px;
+		}
+
+		.tweetbox form{
+   			display: flex;
+    		flex-direction: column;
+		}
+
+		.tweetbox form{
+   			display: flex;
+    		flex-direction: column;
+		}
+
+		.tweetbox__input input{
+			color:black;
+			width: 50%;
+    		flex: 1;
+    		margin-left: 20px;
+    		font-size: 20px;
+    		border: none;
+    		outline: none;
+		}
+
+		.tweetbox__tweetbutton{
+    		border: none;
+    		color: white;
+    		font-weight: 900;
+    		border-radius: 30px;
+    		width: 80px;
+    		height: 40px;
+    		margin-top: 20px;
+    		margin-left: auto;
+		}
+		
+		input{
+			color: white;
+		}
+
+		input[type=submit]{
+			width: 100px;
+			background-color:red;
+		}
+
+		::placeholder{
+			color:darkgrey;
+		}
+
+		.title{
+			color:white;
+		}
+
 	</style>
 </head>
+
 <body>
-knight life 
 
-
+<div class="title">
+Knight Life
+</div>
 
 
 <?php
@@ -52,18 +137,26 @@ else echo '<a href="frontdoor.php">login/create account</a>';
 
 <hr>
 
-
-
-
-
 <?php
 
 
 
 if($_SESSION["login"]>-1){
 
-	echo '<form action="post.php" method="get"><input name="post" type="text"><br>
-<input type="submit" value="post"></form>';
+	echo '
+		<div class="tweetbox">
+			<form action="post.php" method="get">
+				<div class="tweetbox__input">
+					<input name="post" type="text" placeholder="What would you like to say?">
+					<br>
+					
+					<input type="submit" value="post">
+					
+				</div>
+			</form>
+		</div>
+		';
+
 }else echo "you must login to post <br>";
 
 
@@ -75,40 +168,48 @@ if($_SESSION["login"]>-1){
 
 
 
-		echo '<div class = "post">';
-		
-		$name = getUserNamebyId($row['userID']);
+		//echo '<div class = "post">';
 		/*print "username:".$name."<br>";
 		print "post:".$row['postContent']."<br>";
 		print "date:".$row['postTime']."<br>";
 		print "likes:".$row['likes']."<br>";*/
-		echo '<div class="user"><a href="userpage.php?id='.$row['userID'].'">@'.$name.'</a></div>';
-		print '<div class="postContent">' .$row['postContent'].'</div>';
-		print '<div class="date">' .$row['postTime'].'</div>';
-		echo '</div>';
-		/*
-		
-		
-		
-	
-			*/
 
+		$name = getUserNamebyId($row['userID']);
+
+		print '<div class="posts">';
+		print '<div class="post__body">';
+		print '<div class="post__header">';
+		echo '<div class="post__user"><a href="userpage.php?id='.$row['userID'].'">@'.$name.'</a></div>';
+		print '<div class="post__content">' .$row['postContent'].'</div>';
+		print '<div class="postTime">' .$row['postTime'].'</div>';
+		print '<div class="post__Footer">
+					<button class="material-symbols-outlined"> &#xE87D;  
+					</button>
+					'.$row['likes'].'
+				</div>';
+		print '<div class="post__line"></div>';
 	}
 ?>
 
-<div class = "post">
-	<table>
-		<tr>
-			<td style="background-color:#000;color:white;">100</td>
-			<td>
-				
-
-	<div class="user">@lazyBean</div>
-	<div class="postContent">abababab</div>
-	<div class="date">1:59 PM 2/1/2023</div>
-
-			</td>	
-		</tr>
-	</table>
-</div>
+<!--
+<div class="post">
+            <div class="post__body">
+                <div class="post__header">
+                    <div class="post__user">
+                        <h3>Username</h3>
+                    </div>
+                    <div class="post__content">
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. At officiis quis ea dicta quisquam porro, molestiae obcaecati enim natus totam inventore, labore nemo minima harum eius ipsum omnis, recusandae optio.
+                        </p>
+                    </div>
+                </div>
+				<div class="postTime">
+					12:00pm Feb. 1st 2023
+				</div>
+                <div class="post__Footer">
+					<span class="material-symbols-outlined"> &#xE87D;</span> 0
+                </div>
+            </div>
+        </div>
 </body>
